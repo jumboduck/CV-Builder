bcdaa1f3ea62523afafbbcdff1f1de23a09c3b78
Simon Castagna <simoncastagna@Simons-MacBook-Pro.local>
2020-05-26 13:37:48 +0200

    removed header background change from javascript to css and added css transition

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -552,19 +552,16 @@ function changeActiveNav(nav) {
 function changeTheme(theme) {
     let themeClasses = themes.join(" ");
     $("#printable").removeClass(themeClasses).addClass(theme);
+    $(".custom-header").removeClass(themeClasses).addClass(theme);
     usedTheme = theme;
     if (theme === "default-theme") {
         changeActiveNav($("#default-theme-link"));
-        $(".custom-header").css("background-color", "#606060");
     } else if (theme === "theme-modern") {
         changeActiveNav($("#theme-modern-link"));
-        $(".custom-header").css("background-color", "#1e3f5a");
     } else if (theme === "theme-lavender") {
         changeActiveNav($("#theme-lavender-link"));
-        $(".custom-header").css("background-color", "#595775");
     } else if (theme === "theme-deco") {
         changeActiveNav($("#theme-deco-link"));
-        $(".custom-header").css("background-color", "#132226");
     }
 }
 
